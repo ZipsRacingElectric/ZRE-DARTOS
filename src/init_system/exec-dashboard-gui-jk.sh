d60d25786080852e13b2d6619339c19794fa5137
Cole Barach <colebarach@tutanota.com>
2026-02-12 14:23:19 -0500

    Bug fix on Jacob's version of the dashboard. Fixed the init-system taking waaay to long to init when ethernet is unconnected.

--- src/init_system/exec-dashboard-gui-jk.sh
+++ src/init_system/exec-dashboard-gui-jk.sh
@@ -4,4 +4,4 @@
 
 # Note: Do not forget the 'exec' command. This command replaces the current shell instance with the process to execute, meaning
 # all received signals are forwarded to the new process.
-exec cage -- $ZRE_CANTOOLS_DIR/bin/dashboard-gui app can0@1000000 $ZRE_CANTOOLS_DIR/config/zr25_glory/can_vehicle.dbc
+exec cage -- $ZRE_CANTOOLS_DIR/bin/dashboard-gui-jk app can0@1000000 $ZRE_CANTOOLS_DIR/config/zr25_glory/can_vehicle.dbc
