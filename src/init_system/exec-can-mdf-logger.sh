64443e0020e4ac83596a662e69e3b233fe8d8c26
Cole Barach <colebarach@tutanota.com>
2026-03-18 12:21:16 -0400

    can-mdf-logger no longer floods the system journal with status messages.

--- src/init_system/exec-can-mdf-logger.sh
+++ src/init_system/exec-can-mdf-logger.sh
@@ -3,4 +3,4 @@
 # Note: Do not forget the 'exec' command. This command replaces the current shell instance with the process to execute, meaning
 # all received signals are forwarded to the new process.
 # TODO(Barach): Multiple CAN channels
-exec $ZRE_CANTOOLS_DIR/bin/can-mdf-logger $ZRE_CANTOOLS_LOGGING_DIR $DART_CONFIG/can_mdf_logger.json can0@1000000
+exec $ZRE_CANTOOLS_DIR/bin/can-mdf-logger --quiet $ZRE_CANTOOLS_LOGGING_DIR $DART_CONFIG/can_mdf_logger.json can0@1000000
