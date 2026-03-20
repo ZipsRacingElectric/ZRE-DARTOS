838d207659453c8ddcb67e182d53d50a590497ec
Cole Barach <colebarach@tutanota.com>
2026-03-20 08:43:56 -0400

    Updated the init-system to forcefully halt the system, rather than performing a regular shutdown. With this, the system now appears to power off before the SoM fully loses power.

--- src/init_system/post-exec.sh
+++ src/init_system/post-exec.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-# Shutdown the OS
-shutdown now
+# TODO(Barach): Mounting the root filesystem as readonly will help prevent file corruption.
+
+# Halt the operating system forcefully.
+# - This sends the kill signal to all system services and unmounts all filesystems.
+# - This does not physically power off the device, just halts all CPU action.
+systemctl halt --force
